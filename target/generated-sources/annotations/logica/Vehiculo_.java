6a179a91f61a3f52d6a46437accd01e2b52ea9cf
TongaRAM <anthony.morales03@epn.edu.ec>
2024-10-06 12:20:01 -0500

    Se agrego proyecto Poli-Park

--- target/generated-sources/annotations/logica/Vehiculo_.java
+++ target/generated-sources/annotations/logica/Vehiculo_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-10-06T11:03:47")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-10-06T12:17:39")
 @StaticMetamodel(Vehiculo.class)
 public class Vehiculo_ { 
 
